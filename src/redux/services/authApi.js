cae89643672ae2018ecf49721b0ae11cafd6a6b7
sajib hossen <sajibhossen560@gmail.com>
2026-02-14 16:11:11 +0600

    try to setup login rtk query some work

---
+++ src/redux/services/authApi.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const authApi = createApi({
+  reducerPath: 'authApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://10.19.193.14:3000/api',
+    prepareHeaders: (headers) => {
+      const token = localStorage.getItem('token')
+      if (token) {
+        headers.set('authorization', `Bearer ${token}`)
+      }
+      return headers
+    },
+  }),
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: '/auth/login',
+        method: 'POST',
+        body: data,
+      }),
+    }),
+  }),
+})
+
+export const {useLoginMutation} = authApi
