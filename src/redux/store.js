d7f599ab0bd35c68e9f705ffe854e3638bc2e222
sajib hossen <sajibhossen560@gmail.com>
2026-02-16 20:02:24 +0600

    add to blog api in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authApi } from "./services/authApi";
+import { blogApi } from "./services/blogApi";
 
 export const store = configureStore({
     reducer:{
-    [authApi.reducerPath] : authApi.reducer
+    [authApi.reducerPath] : authApi.reducer,
+    [blogApi.reducerPath] : blogApi.reducer
     },
     middleware : (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(authApi.middleware)
+        getDefaultMiddleware().concat(authApi.middleware , blogApi.middleware)
 })
